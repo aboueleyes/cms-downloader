118c0d42f2664284a87b0f33b4f908a8298a0e87
aboueleyes <ibrahimabouelanin@gmail.com>
2021-04-08 00:02:42 +0200

    implement File class

---
+++ CmsFile.py
@@ -0,0 +1,9 @@
+class DownloadFile:
+
+    def __init__(self):
+        self.name = ''
+        self.url = ''
+        self.ext = self.url.rsplit('.')[1]
+        self.discreption = ''
+        self.week = ''
+        self.course = ''
