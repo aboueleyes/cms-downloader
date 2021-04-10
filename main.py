c3b0205185b24fd7a8a6393248b3d45464accdb8
aboueleyes <ibrahimabouelanin@gmail.com>
2021-04-10 18:51:04 +0200

    -n for course annoenecment

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main():
                         action='store_true', default=False)
     praser.add_argument('-f', '--filter', help='display only new files',
                         action='store_true', default=False)
-
+    praser.add_argument('-n','--new',help='display annoencment of the course',action='store_true',default=False)
     args = praser.parse_args()
 
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -58,11 +58,11 @@ def main():
                 download_files(files.list, username, password, pdf=True)
     else:
         course_url, course = choose_course(courses_name, course_links)
-         # print(item forcitem in get_announcments(get_course_soup(course_url, username, password, session)))
-        for item in get_announcments(get_course_soup(course_url, username, password, session)):
-            print(item,end=" ")
-        print()
-        sys.exit(0)
+        if args.new:
+            for item in get_announcments(get_course_soup(course_url, username, password, session)):
+                print(item,end=" ")
+            print()
+            sys.exit(0)
         files = get_files(course_url, username, password, session)
         for item in files.list:
             item.course = course
